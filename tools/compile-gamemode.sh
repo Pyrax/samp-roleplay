85785dca3db7f222a44926e6b033a54825088ca9
Pyrax <piranhadev@gmail.com>
2014-09-19 22:24:24 +0200

    Fix path

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -8,6 +8,6 @@ export LD_LIBRARY_PATH="../tools/pawnc:$LD_LIBRARY_PATH"
 
 pushd $TOOLSPATH/../gamemodes > /dev/null
 
-../tools/pawnc/pawncc roleplay.pwn -i./pawno/include -\; -\( -\Z
+../tools/pawnc/pawncc roleplay.pwn -i./../pawno/include -\; -\( -\Z
 
 popd > /dev/null
