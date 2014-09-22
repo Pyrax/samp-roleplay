8963ac30f4a9393c56dc162c1405787f6ded3799
Pyrax <piranhadev@gmail.com>
2014-09-22 14:23:21 +0200

    Update compile-gamemode.sh

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -4,10 +4,10 @@ set -e
 
 TOOLSPATH=$(dirname $0)
 
-export LD_LIBRARY_PATH="tools/pawnc:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="tools/pawnc"
 
 pushd $TOOLSPATH/.. > /dev/null
 
-sudo tools/pawnc/pawncc gamemodes/roleplay.pwn -i./pawno/include -\; -\( -\Z
+tools/pawnc/pawncc gamemodes/roleplay.pwn -i./pawno/include -\; -\( -\Z
 
 popd > /dev/null
